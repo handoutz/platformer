22277786c41ed10cd7a7aaf27fa816a606bcf09b
Vince <innatepirate@gmail.com>
2023-04-12 15:45:03 -0400

    oops

---
+++ PlatformerGame.Engine/Game/Actors/NpcActor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatformerGame.Engine.Game.Actors
+{
+    public class NpcActor : IActor
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Color Color { get; set; }
+        public Velocity CurrentVelocity { get; set; }
+        public void OnFrame(EngineStateUpdate state)
+        {
+            Engine.Instance.OnLogEvent($"{X} {Y}");
+            CurrentVelocity = new(0, -1, 0, 1000000000);
+        }
+
+        public void OnProcessKey(KeyEvent keyEvent)
+        {
+            
+        }
+
+        public void SetVelocity(Velocity v)
+        {
+            
+        }
+    }
+}
